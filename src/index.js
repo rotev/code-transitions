f08f1327f6384ab62f66db0d048458d180d9508d
Rotem Harel <rotev@github.com>
2021-12-26 14:45:45 +0000

    hello react

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom";
+import { App } from "./app";
+
+import "./styles.css";
+
+const app = document.getElementById("app");
+ReactDOM.render(<App />, app);
--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import "./styles.css";
-import { doThings } from './code-transition/git-diff'
-
-document.getElementById("display").innerHTML = "bye";
-
-doThings();
