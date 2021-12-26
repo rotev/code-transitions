28d6e7ce8507d17bc86a3c8c628715180bcfc771
Rotem Harel <rotev@github.com>
2021-12-26 15:49:38 +0000

    click around

--- src/components/presentation.jsx
+++ src/components/presentation.jsx
@@ -1,14 +1,29 @@
+import {useEffect, useState} from 'react'
 import PresentationContext from '../contexts/presentation-context'
 
 import Slide from './slide'
 
-const defaultContext = {
-  slide: 1
-}
 
 export default Presentation = () => {
+  const [slide, setSlide] = useState(1)
+
+  const context = {
+    slide
+  }
+
+  useEffect(() => {
+    const onClick = () => {
+      setSlide(slide + 1)
+    }
+    window.addEventListener('click', onClick)
+
+    return () => {
+      window.removeEventListener('click', onClick)
+    }
+  })
+
   return (
-    <PresentationContext.Provider value={defaultContext}>
+    <PresentationContext.Provider value={context}>
       <Slide />
     </PresentationContext.Provider>
   )
